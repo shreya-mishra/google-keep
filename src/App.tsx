aa94b037e97de5f841799eb7cc2dac424af95035
shreya-mishra <shreyamishra040398@gmail.com>
2023-12-07 20:16:52 +0530

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "../module/index/screens/HomeScreen";
+import HomeScreen from "./module/index/screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
